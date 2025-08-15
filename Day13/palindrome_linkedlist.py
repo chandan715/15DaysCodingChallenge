c3828b76e62dd243895916e99b0ba7eeb1abdc35
Chandan Nayak <nayakchandan0271@gmail.com>
2025-08-15 22:31:02 +0530

    Day 13 Here is the solution for anothere leetcode problems

---
+++ Day13/palindrome_linkedlist.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        prev = None
+        curr = slow
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        first_half, second_half = head, prev
+        while second_half:
+            if first_half.val != second_half.val:
+                return False
+            first_half = first_half.next
+            second_half = second_half.next
+        return True
+        
